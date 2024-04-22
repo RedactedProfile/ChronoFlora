11bb22603f47e56541b622114178c682f99f1f44
Kyle Harrison <redactedprofile@gmail.com>
2024-04-22 14:50:33 -0700

    libgdx frmaework created

---
+++ desktop/src/com/ninjaghost/gamejam/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.ninjaghost.gamejam;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.ninjaghost.gamejam.ChronoFloraGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("ChronoFlora");
+		new Lwjgl3Application(new ChronoFloraGame(), config);
+	}
+}
