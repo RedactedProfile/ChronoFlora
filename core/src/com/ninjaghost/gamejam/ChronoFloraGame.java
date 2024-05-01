dccb148b6c51df2684505b09c9de59cc286fa2ed
Kyle Harrison <redactedprofile@gmail.com>
2024-05-01 13:51:21 -0700

    trying to add a UI to the game (even for dev stuff)

--- core/src/com/ninjaghost/gamejam/ChronoFloraGame.java
+++ core/src/com/ninjaghost/gamejam/ChronoFloraGame.java
@@ -2,11 +2,17 @@ package com.ninjaghost.gamejam;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +21,10 @@ import java.util.Map;
 public class ChronoFloraGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	BitmapFont bitmapFont;
+
+	Stage uiStage;
+	Skin uiSkin;
 
 	OrthographicCamera camera;
 	float cameraZoomFactor = 1f;
@@ -60,10 +70,20 @@ public class ChronoFloraGame extends ApplicationAdapter {
 												8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8, 8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,0
 		);
 
-	
+	// UI Widgets
+	// Draw the UI
+	TextField _field;
+
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		bitmapFont = new BitmapFont();
+		uiStage = new Stage(new ScreenViewport());
+
+		// THis is to let the stage object handle input
+		// Gdx.input.setInputProcessor(uiStage);
+		uiSkin = new Skin(Gdx.files.internal("uiskin.json"));
 
 		camera = new OrthographicCamera();
 		camera.setToOrtho(true, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
@@ -74,18 +94,33 @@ public class ChronoFloraGame extends ApplicationAdapter {
 			tiles.add(new Tile(_file.path()));
 		}
 
+
+		_field = new TextField("Hi", uiSkin);
+		_field.setPosition(100, 100);
+		_field.setSize(200, 50);
+		uiStage.addActor(_field);
 	}
 
 	@Override
 	public void render () {
-		cameraZoomFactor -= 0.02f * Gdx.graphics.getDeltaTime();
 		camera.update();
+
+		// Control Camera Zoom
+		float zoomDelta = 0f;
+		if(Gdx.input.isKeyPressed(Input.Keys.UP)) {
+			zoomDelta = 0.02f;
+		} else if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+			zoomDelta = -0.02f;
+		}
+		cameraZoomFactor -= zoomDelta * Gdx.graphics.getDeltaTime();
 		camera.zoom = cameraZoomFactor;
+
+		// Clear and Setup camera projection
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		batch.draw(img, 0, 0);
 
+		// Draw the map tiles
+		batch.begin();
 		int row = 0;
 		int column = 0;
         for (Number number : tilemap) {
@@ -99,8 +134,13 @@ public class ChronoFloraGame extends ApplicationAdapter {
 
             column++;
         }
-
 		batch.end();
+
+
+
+		// Render the UI
+		uiStage.act(Gdx.graphics.getDeltaTime());
+		uiStage.draw();
 	}
 	
 	@Override
