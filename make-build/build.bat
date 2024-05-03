7e46268bf60feb995fdf4bb835ad367ceee3f8dc
Kyle Harrison <redactedprofile@gmail.com>
2024-05-02 21:20:30 -0700

    packr setup

---
+++ make-build/build.bat
@@ -0,0 +1,4 @@
+@echo off
+java -jar C:\tools\bin\packr-all-4.0.0.jar .\packr-config-win.json
+java -jar C:\tools\bin\packr-all-4.0.0.jar .\packr-config-mac.json
+java -jar C:\tools\bin\packr-all-4.0.0.jar .\packr-config-linux.json
