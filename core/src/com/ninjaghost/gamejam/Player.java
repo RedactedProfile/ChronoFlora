2378eddef8b6feed31ad04798366b11453023f02
Kyle Harrison <redactedprofile@gmail.com>
2024-05-04 16:11:05 -0700

    setting up animation bank system for player

--- core/src/com/ninjaghost/gamejam/Player.java
+++ core/src/com/ninjaghost/gamejam/Player.java
@@ -1,9 +1,14 @@
 package com.ninjaghost.gamejam;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
+
+import java.util.HashMap;
+import java.util.Map;
 
 final public class Player {
 
@@ -18,14 +23,27 @@ final public class Player {
     public float moveSpeed = 25f;
     public float movementVelocity = 0f;
 
+
+    private TextureRegion[] frames = new TextureRegion[4];
+    private Animation<TextureRegion> frameAnimation;
+    private float animationTimer = 0f;
+
+    private Map<String, Map<String, String>> animationBank;
+
     public Player() {
         targetPosition.set(150, 150);
         currentPosition.set(150, 150);
-        m_sprite = new Sprite(new Texture("player/idle/idle_d_00.png"));
+
+        load();
+
+        m_sprite = new Sprite(frames[0]);
         m_sprite.flip(false, true);
     }
 
+
     public void update(float delta, GameplayInputState gameplayInputState) {
+        animationTimer += delta;
+
         Vector2 newPosition = new Vector2(targetPosition.x, targetPosition.y);
         float normalizedMoveSpeed = moveSpeed * delta;
 
@@ -42,10 +60,43 @@ final public class Player {
 
         lookAt.x = targetPosition.x + m_sprite.getWidth() / 2;
         lookAt.y = targetPosition.y + m_sprite.getHeight() / 2;
+
+        m_sprite.setRegion(frameAnimation.getKeyFrame(animationTimer, true));
+        m_sprite.flip(false, true);
     }
 
     public void draw(SpriteBatch batch) {
         m_sprite.draw(batch);
     }
 
+    private void load() {
+
+        frames = new TextureRegion[] {
+                new TextureRegion(new Texture("player/idle/idle_d_00.png")),
+                new TextureRegion(new Texture("player/idle/idle_d_01.png")),
+                new TextureRegion(new Texture("player/idle/idle_d_02.png")),
+                new TextureRegion(new Texture("player/idle/idle_d_03.png")),
+        };
+
+        frameAnimation = new Animation<>(0.5f, frames);
+
+
+        animationBank = Map.of(
+                "idle", Map.of(
+                        "down", "idling down"
+                ),
+                "walk", Map.of(
+                        "down", "walking down"
+                )
+        );
+
+        System.out.printf("Anim state: %s:%s = %s%n", "idle", "down", animationBank.get("idle").get("down"));
+
+//        animationBank["idle"]["down"] = new TextureRegion[] {};
+
+//        animationBank.put("idle", Map.of(
+//                { "down", new TextureRegion[] {} }
+//        ));
+
+    }
 }
